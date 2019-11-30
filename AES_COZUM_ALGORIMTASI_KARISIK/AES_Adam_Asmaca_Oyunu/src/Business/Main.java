2fe70f4a08c02dd5f8c92df9f42f14099e56c2e4
AhmetEminSaglik <ahmeteminyazilim@gmail.com>
2019-11-30 09:38:59 +0200

    eksik dosyaları da atıyorum

---
+++ AES_COZUM_ALGORIMTASI_KARISIK/AES_Adam_Asmaca_Oyunu/src/Business/Main.java
@@ -0,0 +1,10 @@
+package Business;
+
+import Gui.MainGui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainGui maingui = new MainGui();
+    }
+}
---
+++ AES_COZUM_ALGORIMTASI_KARISIK/AES_Adam_Asmaca_Oyunu/src/Gui/Kelime_classi.java
@@ -0,0 +1,9 @@
+package Gui;
+
+import java.util.ArrayList;
+
+public class Kelime_classi {
+
+    ArrayList kelime = new ArrayList<String>();
+    
+}
